536f1a70fbf914c53941cc3459df21aaeffae254
Todor Iliev <todor@itprism.com>
2013-04-27 12:20:56 +0300

    pushed version 1.3 for Joomla! 2.5

--- README.md
+++ README.md
@@ -6,4 +6,22 @@ ITPGoogleAdSense Plugin for Joomla!
 Changelog
 ---------
 
+v1.3
+----
+* Now it works with 
+    * Joomla! Content(com_content)
+    * K2(com_k2)
+    * VirtueMart(com_virtuemart)
+    * JEvents (com_jevents)
+    * EasyBlog ( com_easyblog )
+    * MyBlog ( com_myblog )
+    * Vip Portfolio PRO ( com_vipportfolio )
+    * HikaShop ( com_hikashop )
+    * JoomShopping ( com_jshopping )
+    * ZOO ( com_zoo )
+* Improved translation
+* Added new kinds of ads - "Large Leaderboard ", "Large Skyscraper" 
+
+v1.2
+----
 * Improved 
--- itpgoogleadsense.php
+++ itpgoogleadsense.php
@@ -1,48 +1,25 @@
 <?php
 /**
- * @package		 ITPGoogleAdSense Plugins
- * @subpackage	 Advertisement
+ * @package		 ITPrism Plugins
+ * @subpackage	 ITPGoogleAdSense
  * @copyright    Copyright (C) 2010 Todor Iliev <todor@itprism.com>. All rights reserved.
  * @license      http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * ITPShare is free software. This version may have been modified pursuant
+ * ITPGoogleAdSense is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
 
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');
+// no direct access
+defined('_JEXEC') or die;
 
 jimport('joomla.plugin.plugin');
 
-/**
- * ITPGoogleAdSense Plugin
- *
- * @package		ITPrism Plugins
- * @subpackage	Advertisement
- * @since 		1.5
- */
 class plgContentITPGoogleAdSense extends JPlugin {
     
     private $currentView    = "";
     private $currentTask    = "";
     private $currentOption  = "";
-    
-    public function __construct($subject, $params){
-        parent::__construct($subject, $params);
-        
-        $app = JFactory::getApplication();
-        /* @var $app JApplication */
-
-        if($app->isAdmin()) {
-            return;
-        }
-      
-        $this->currentView    = JRequest::getCmd("view");
-        $this->currentTask    = JRequest::getCmd("task");
-        $this->currentOption  = JRequest::getCmd("option");
-        
-    }
     
     /**
      * Adds Google AdSense into articles
@@ -57,10 +34,10 @@ class plgContentITPGoogleAdSense extends JPlugin {
      */
     public function onContentPrepare($context, &$article, &$params, $limitstart){
         
-        if (!$article OR empty($article->id) OR !isset($this->params)) { return; };            
+        if (!$article OR !isset($this->params)) { return; };        
         
-        $app =& JFactory::getApplication();
-        /** @var $app JApplication **/
+        $app = JFactory::getApplication();
+        /** @var $app JSite **/
 
         if($app->isAdmin()) {
             return;
@@ -68,14 +45,13 @@ class plgContentITPGoogleAdSense extends JPlugin {
         
         $doc     = JFactory::getDocument();
         /**  @var $doc JDocumentHtml **/
-        $docType = $doc->getType();
         
-        // Check document type
-        if(strcmp("html", $docType) != 0){
-            return;
-        }
-       
-        if($this->isRestricted($article, $context)) {
+        // Get request data
+        $this->currentOption  = $app->input->getCmd("option");
+        $this->currentView    = $app->input->getCmd("view");
+        $this->currentTask    = $app->input->getCmd("task");
+        
+        if($this->isRestricted($article, $context, $params)) {
         	return;
         }
         
@@ -98,80 +74,7 @@ class plgContentITPGoogleAdSense extends JPlugin {
         return true;
     }
     
-    private function isRestricted($article, $context) {
-    	
-    	$result = false;
-    	
-    	switch($this->currentOption) {
-            case "com_content":
-            	
-        		if($this->isContentRestricted($article, $context)) {
-                    $result = true;
-                }
-                break;
-	                
-            default:
-                $result = true;
-                break;   
-        }
-        
-        return $result;
-        
-    }
-    
-	/**
-     * 
-     * Checks allowed articles, exluded categories/articles,... for component COM_CONTENT
-     * @param object $article
-     * @param string $context
-     */
-    private function isContentRestricted(&$article, $context) {
-        
-        // Check for currect context
-        if(strpos($context, "com_content") === false) {
-           return true;
-        }
-        
-    	/** Check for selected views, which will display the buttons. **/   
-        /** If there is a specific set and do not match, return an empty string.**/
-        $showInArticles     = $this->params->get('showInArticles');
-        if(!$showInArticles AND (strcmp("article", $this->currentView) == 0)){
-            return true;
-        }
-        
-        $excludeArticles = $this->params->get('excludeArticles');
-        if(!empty($excludeArticles)){
-            $excludeArticles = explode(',', $excludeArticles);
-        }
-        settype($excludeArticles, 'array');
-        JArrayHelper::toInteger($excludeArticles);
-        
-        // Exluded categories
-        $excludedCats           = $this->params->get('excludeCats');
-        if(!empty($excludedCats)){
-            $excludedCats = explode(',', $excludedCats);
-        }
-        settype($excludedCats, 'array');
-        JArrayHelper::toInteger($excludedCats);
-        
-        // Included Articles
-        $includedArticles = $this->params->get('includeArticles');
-        if(!empty($includedArticles)){
-            $includedArticles = explode(',', $includedArticles);
-        }
-        settype($includedArticles, 'array');
-        JArrayHelper::toInteger($includedArticles);
-        
-        if(!in_array($article->id, $includedArticles)) {
-            // Check exluded articles
-            if(in_array($article->id, $excludeArticles) OR in_array($article->catid, $excludedCats)){
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
+
     /**
      * Generate content
      * @param   object      The article object.  Note $article->text is also available
@@ -215,5 +118,429 @@ src="//pagead2.googlesyndication.com/pagead/show_ads.js">
 </script></div>';
 
     }
+    
+    
+    private function isRestricted($article, $context, $params) {
+    	
+    	$result = false;
+    	
+    	switch($this->currentOption) {
+            case "com_content":
+            	
+            	// It's an implementation of "com_myblog"
+            	// I don't know why but $option contains "com_content" for a value
+            	// I hope it will be fixed in the future versions of "com_myblog"
+            	if(strcmp($context, "com_myblog") != 0) {
+                    $result = $this->isContentRestricted($article, $context);
+	                break;
+            	} 
+	                
+            case "com_myblog":
+                $result = $this->isMyBlogRestricted($article, $context);
+                break;
+                    
+            case "com_k2":
+                $result = $this->isK2Restricted($article, $context, $params);
+                break;
+                
+            case "com_virtuemart":
+                $result = $this->isVirtuemartRestricted($article, $context);
+                break;
+
+            case "com_jevents":
+                $result = $this->isJEventsRestricted($article, $context);
+                break;
+
+            case "com_easyblog":
+                $result = $this->isEasyBlogRestricted($article, $context);
+                break;
+                
+            case "com_vipportfolio":
+                $result = $this->isVipPortfolioRestricted($article, $context);
+                break;
+                
+            case "com_zoo":
+                $result = $this->isZooRestricted($article, $context);
+                break;    
+                
+             case "com_jshopping":
+                $result = $this->isJoomShoppingRestricted($article, $context);
+                break;  
+
+            case "com_hikashop":
+                $result = $this->isHikaShopRestricted($article, $context);
+                break; 
+                
+            default:
+                $result = true;
+                break;   
+        }
+        
+        return $result;
+        
+    }
+    
+	/**
+     * 
+     * Checks allowed articles, exluded categories/articles,... for component COM_CONTENT
+     * @param object $article
+     * @param string $context
+     */
+    private function isContentRestricted(&$article, $context) {
+        
+        // Check for correct context
+        if(( strpos($context, "com_content") === false ) OR empty($article->id)) {
+           return true;
+        }
+        
+    	/** Check for selected views, which will display the buttons. **/   
+        /** If there is a specific set and do not match, return an empty string.**/
+        $showInArticles     = $this->params->get('showInArticles');
+        if(!$showInArticles AND (strcmp("article", $this->currentView) == 0)){
+            return true;
+        }
+        
+        // Will be displayed in view "categories"?
+        $showInCategories   = $this->params->get('showInCategories');
+        if(!$showInCategories AND (strcmp("category", $this->currentView) == 0)){
+            return true;
+        }
+        
+        // Will be displayed in view "featured"?
+        $showInFeatured   = $this->params->get('showInFeatured');
+        if(!$showInFeatured AND (strcmp("featured", $this->currentView) == 0)){
+            return true;
+        }
+        
+        // Exclude articles
+        $excludeArticles = $this->params->get('excludeArticles');
+        if(!empty($excludeArticles)){
+            $excludeArticles = explode(',', $excludeArticles);
+        }
+        settype($excludeArticles, 'array');
+        JArrayHelper::toInteger($excludeArticles);
+        
+        // Exluded categories
+        $excludedCats           = $this->params->get('excludeCats');
+        if(!empty($excludedCats)){
+            $excludedCats = explode(',', $excludedCats);
+        }
+        settype($excludedCats, 'array');
+        JArrayHelper::toInteger($excludedCats);
+        
+        // Included Articles
+        $includedArticles = $this->params->get('includeArticles');
+        if(!empty($includedArticles)){
+            $includedArticles = explode(',', $includedArticles);
+        }
+        settype($includedArticles, 'array');
+        JArrayHelper::toInteger($includedArticles);
+        
+        if(!in_array($article->id, $includedArticles)) {
+            // Check exluded articles
+            if(in_array($article->id, $excludeArticles) OR in_array($article->catid, $excludedCats)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+/**
+     * 
+     * This method does verification for K2 restrictions
+     * @param jIcalEventRepeat $article
+     * @param string $context
+     */
+    private function isK2Restricted(&$article, $context, $params) {
+        
+        // Check for correct context
+        if(strpos($context, "com_k2") === false) {
+           return true;
+        }
+        
+        if($article instanceof TableK2Category){
+            return true;
+        }
+        
+        $displayInItemlist     = $this->params->get('k2DisplayInItemlist', 0);
+        if(!$displayInItemlist AND (strcmp("itemlist", $this->currentView) == 0)){
+            return true;
+        }
+        
+        $displayInArticles     = $this->params->get('k2DisplayInArticles', 0);
+        if(!$displayInArticles AND (strcmp("item", $this->currentView) == 0)){
+            return true;
+        }
+        
+        // Exclude articles
+        $excludeArticles = $this->params->get('k2_exclude_articles');
+        if(!empty($excludeArticles)){
+            $excludeArticles = explode(',', $excludeArticles);
+        }
+        settype($excludeArticles, 'array');
+        JArrayHelper::toInteger($excludeArticles);
+        
+        // Exluded categories
+        $excludedCats           = $this->params->get('k2_exclude_cats');
+        if(!empty($excludedCats)){
+            $excludedCats = explode(',', $excludedCats);
+        }
+        settype($excludedCats, 'array');
+        JArrayHelper::toInteger($excludedCats);
+        
+        // Included Articles
+        $includedArticles = $this->params->get('k2_include_articles');
+        if(!empty($includedArticles)){
+            $includedArticles = explode(',', $includedArticles);
+        }
+        settype($includedArticles, 'array');
+        JArrayHelper::toInteger($includedArticles);
+        
+        if(!in_array($article->id, $includedArticles)) {
+            // Check exluded articles
+            if(in_array($article->id, $excludeArticles) OR in_array($article->catid, $excludedCats)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * 
+     * Do verifications for JEvent extension
+     * @param jIcalEventRepeat $article
+     * @param string $context
+     */
+    private function isJEventsRestricted(&$article, $context) {
+        
+        // Display buttons only in the description
+        if (!is_a($article, "jIcalEventRepeat")) { 
+            return true; 
+        };
+        
+        // Check for correct context
+        if(strpos($context, "com_jevents") === false) {
+           return true;
+        }
+
+        // Display only in task 'icalrepeat.detail'
+        if(strcmp("icalrepeat.detail", $this->currentTask) != 0) {
+           return true;
+        }
+        
+        $displayInEvents     = $this->params->get('jeDisplayInEvents', 0);
+        if(!$displayInEvents){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * 
+     * This method does verification for VirtueMart restrictions
+     * @param stdClass $article
+     * @param string $context
+     */
+    private function isVirtuemartRestricted(&$article, $context) {
+            
+        // Check for correct context
+        if(strpos($context, "com_virtuemart") === false) {
+           return true;
+        }
+        
+        // Display content only in the view "productdetails"
+        if(strcmp("productdetails", $this->currentView) != 0){
+            return true;
+        }
+        
+        // Display content only in the view "productdetails"
+        $displayInDetails     = $this->params->get('vmDisplayInDetails', 0);
+        if(!$displayInDetails){
+            return true;
+        }
+        
+        return false;
+    }
+    
+	/**
+     * 
+     * It's a method that verify restriction for the component "com_myblog"
+     * @param object $article
+     * @param string $context
+     */
+	private function isMyBlogRestricted(&$article, $context) {
+
+        // Check for correct context
+        if(strpos($context, "myblog") === false) {
+           return true;
+        }
+        
+	    // Display content only in the task "view"
+        if(strcmp("view", $this->currentTask) != 0){
+            return true;
+        }
+        
+        if(!$this->params->get('mbDisplay', 0)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+	/**
+     * 
+     * It's a method that verify restriction for the component "com_vipportfolio"
+     * @param object $article
+     * @param string $context
+     */
+	private function isVipPortfolioRestricted(&$article, $context) {
+
+        // Check for correct context
+        if(strpos($context, "com_vipportfolio") === false) {
+           return true;
+        }
+        
+	    // Verify the option for displaying in layout "lineal"
+        $displayInLineal     = $this->params->get('vipportfolio_lineal', 0);
+        if(!$displayInLineal){
+            return true;
+        }
+        
+        return false;
+    }
+    
+	/**
+     * 
+     * It's a method that verify restriction for the component "com_zoo"
+     * @param object $article
+     * @param string $context
+     */
+	private function isZooRestricted(&$article, $context) {
+	    
+        // Check for correct context
+        if(false === strpos($context, "com_zoo")) {
+           return true;
+        }
+        
+	    // Verify the option for displaying in view "item"
+        $displayInItem     = $this->params->get('zoo_display', 0);
+        if(!$displayInItem){
+            return true;
+        }
+        
+	    // Check for valid view or task
+	    // I have check for task because if the user comes from view category, the current view is "null" and the current task is "item"
+        if( (strcmp("item", $this->currentView) != 0 ) AND (strcmp("item", $this->currentTask) != 0 )){
+            return true;
+        }
+        
+        // A little hack used to prevent multiple displaying of buttons, becaues
+        // if there is more than one textares the buttons will be displayed in everyone.
+        static $numbers = 0;
+        if($numbers == 1) {
+            return true;
+        }
+        $numbers = 1;
+        
+        return false;
+    }
+    
+    /**
+     * 
+     * It's a method that verify restriction for the component "com_easyblog"
+     * @param object $article
+     * @param string $context
+     */
+	private function isEasyBlogRestricted(&$article, $context) {
+        $allowedViews = array("categories", "entry", "latest", "tags");   
+        // Check for correct context
+        if(strpos($context, "easyblog") === false) {
+           return true;
+        }
+        
+        // Only put buttons in allowed views
+        if(!in_array($this->currentView, $allowedViews)) {
+        	return true;
+        }
+        
+   		// Verify the option for displaying in view "categories"
+        $displayInCategories     = $this->params->get('ebDisplayInCategories', 0);
+        if(!$displayInCategories AND (strcmp("categories", $this->currentView) == 0)){
+            return true;
+        }
+        
+   		// Verify the option for displaying in view "latest"
+        $displayInLatest     = $this->params->get('ebDisplayInLatest', 0);
+        if(!$displayInLatest AND (strcmp("latest", $this->currentView) == 0)){
+            return true;
+        }
+        
+		// Verify the option for displaying in view "entry"
+        $displayInEntry     = $this->params->get('ebDisplayInEntry', 0);
+        if(!$displayInEntry AND (strcmp("entry", $this->currentView) == 0)){
+            return true;
+        }
+        
+	    // Verify the option for displaying in view "tags"
+        $displayInTags     = $this->params->get('ebDisplayInTags', 0);
+        if(!$displayInTags AND (strcmp("tags", $this->currentView) == 0)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+	/**
+     * 
+     * It's a method that verify restriction for the component "com_joomshopping"
+     * @param object $article
+     * @param string $context
+     */
+	private function isJoomShoppingRestricted(&$article, $context) {
+        
+        // Check for correct context
+        if(false === strpos($context, "com_content.article")) {
+           return true;
+        }
+        
+	    // Check for enabled functionality for that extension
+        $displayInDetails     = $this->params->get('joomshopping_display', 0);
+        if(!$displayInDetails OR !isset($article->product_id)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+	/**
+     * 
+     * It's a method that verify restriction for the component "com_hikashop"
+     * @param object $article
+     * @param string $context
+     */
+	private function isHikaShopRestricted(&$article, $context) {
+	    
+        // Check for correct context
+        if(false === strpos($context, "text")) {
+           return true;
+        }
+        
+	    // Display content only in the view "product"
+        if(strcmp("product", $this->currentView) != 0){
+            return true;
+        }
+        
+	    // Check for enabled functionality for that extension
+        $displayInDetails     = $this->params->get('hikashop_display', 0);
+        if(!$displayInDetails){
+            return true;
+        }
+        
+        return false;
+    }
+    
     
 }
